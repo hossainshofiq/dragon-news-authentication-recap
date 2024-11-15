e561904992f74456f93cd2a226949c5c9e4019b4
hossainshofiq <ah731710@gmail.com>
2024-11-16 01:53:55 +0600

    project started for authentication

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -10,20 +10,25 @@ const HomeLayout = () => {
     <div className="font-poppins">
       <header>
         <Header></Header>
+        
         <section className="w-11/12 mx-auto ">
           <LatestNews></LatestNews>
         </section>
       </header>
+
       <nav className="w-11/12 mx-auto py-2">
         <Navbar></Navbar>
       </nav>
+
       <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
         <aside className="left col-span-3">
           <LeftNavbar></LeftNavbar>
         </aside>
+
         <section className="col-span-6">
           <Outlet></Outlet>
         </section>
+
         <aside className="col-span-3">
           <RightNav></RightNav>
         </aside>
