8d8e001cfb32e50d651450655d40277526f62cee
hossainshofiq <ah731710@gmail.com>
2024-11-17 01:17:12 +0600

    authprovider codes some changes

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -13,7 +13,7 @@ const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    console.log(user, loading);
+    // console.log(user, loading);
 
     const createNewUser = (email, password) => {
         setLoading(true);
